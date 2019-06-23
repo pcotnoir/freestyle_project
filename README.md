1f6d1e7ad302235f8c7d21eef040a5a76f8d2bbe
pcotnoir <pc2508@stern.nyu.edu>
2019-06-23 19:51:43 -0400

    create .env, update README, update requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # freestyle_project
 Freestyle project
+
+pip install -U googlemaps
+conda activate freestyleproject-env
--- project.py
+++ project.py
@@ -1,28 +1,16 @@
 
 import json
-
-
-#create map
-# import gmaps
-# gmaps.configure(api_key='AIzaSyDaWK8dgNvwiKcxL5RF8xefSlhmzx9XsEQ 	')
-# 
-# 
-# with open('apikey.txt') as f:
-#     api_key = f.readline()
-#     f.close
-# 
-# gmaps = googlemaps.Client(key=api_key)
-# 
-# new_york_coordinates = (40.75, -74.00)
-# gmaps.figure(center=new_york_coordinates, zoom_level=12)
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Google Maps Distance and Time
 
 import googlemaps
 from datetime import datetime
 
-gmaps = googlemaps.Client(key='AIzaSyDaWK8dgNvwiKcxL5RF8xefSlhmzx9XsEQ 	')
+gmaps_api_key = os.environ.get("gmaps_api_key")
+gmaps = googlemaps.Client(key=gmaps_api_key)
 
 
 now = datetime.now()
@@ -35,23 +23,42 @@ directions_result = gmaps.directions("900 President Street Brooklyn, NY 11215",
 
 print(directions_result[0]['legs'][0]['distance']['text'])
 print(directions_result[0]['legs'][0]['duration']['text'])
+print(directions_result[0]['legs'][0]['arrival_time']['text'])
 
 
-# {
-#   origin: '936 President Street Brooklyn, NY 11215',
-#   destination: '1500 Broadway New York, NY 10036',
-#   travelMode: 'TRANSIT',
-#   transitOptions: {
-#    departureTime: new Date(1343641500),
-#    modes: ['SUBWAY'],
-#     routingPreference: 'FEWER_TRANSFERS'
-#   },
-#   unitSystem: google.maps.UnitSystem.IMPERIAL
-# }
-
-# from googlemaps import GoogleMaps
+# import json, urllib, urllib.parse
+# from urllib.parse import urlencode
+# import googlemaps
+# start = "Bridgewater, Sa, Australia"
+# finish = "Stirling, SA, Australia"
 # 
-# mapService = GoogleMaps()
-# directions = mapService.directions('Houston', 'Atlanta')
-# for step in directions['Directions']['Routes'][0]['Steps']:
-#     print step['descriptionHtml']
+# url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
+#             ('origin', start),
+#             ('destination', finish)
+#  ))
+# ur = urllib.request.urlopen(url)
+# result = json.load(ur)
+# 
+# for i in range (0, len (result['routes'][0]['legs'][0]['steps'])):
+#     j = result['routes'][0]['legs'][0]['steps'][i]['html_instructions'] 
+#     print (j)
+
+
+#create map
+# import gmaps
+# import googlemaps
+# import pandas
+# import os
+# import gmaps.datasets
+# gmaps.configure(api_key2= api_key)
+#  
+#  
+# with open('apikey.txt') as f:
+#      api_key = f.readline()
+#      f.close
+#  
+# gmaps = googlemaps.Client(key='AIzaSyDaWK8dgNvwiKcxL5RF8xefSlhmzx9XsEQ 	')
+#  
+# new_york_coordinates = (40.75, -74.00)
+# fig = gmaps.figure(center=new_york_coordinates, zoom_level=12)
+# fig 
