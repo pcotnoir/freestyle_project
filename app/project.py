9bd927e8827f756ce04051ec164c100845caccc1
pcotnoir <pc2508@stern.nyu.edu>
2019-07-01 13:28:06 -0400

    simplify code

--- app/project.py
+++ app/project.py
@@ -37,20 +37,18 @@ while True:
 directions_result = gmaps.directions(origin, destination, mode = directions_route, avoid = "ferries", departure_time=now)
 
 
-
-
 # WEATHER
 
 weather_api_key = os.environ.get("weather_api_key")
 
 while True:
-    zip_code = input("Please enter a 5 digit .zip code for your weather report: ")
+    zip_code = input("Please enter a 5 digit ZIP code for your weather report: ")
     if not zip_code.isdigit():
-        print("Your .zip code contains non-numerical digits. Please try again: ")
+        print("Your ZIP code contains non-numerical digits. Please try again: ")
     if len(zip_code) >5:
-        print("Your .zip code contains too many digits. Please try again: ")
+        print("Your ZIP code contains too many digits. Please try again: ")
     if len(zip_code) <5:
-        print("Your .zip code contains too few digits. Please try again: ")
+        print("Your ZIP code contains too few digits. Please try again: ")
     if zip_code.isdigit() and len(zip_code) == 5:
         break
 
@@ -66,9 +64,6 @@ response_key = forecast_response.json() # https://www.geeksforgeeks.org/python-f
 
 news_api_key = os.environ.get("news_api_key")
 
-
-
-
 #USER OUTPUTS
 
 #Commute Times
@@ -76,14 +71,9 @@ print("\n")
 print("\n")
 print("Commute Information: ")
 while True:
-    if directions_route == "driving" or "bicycling" or "walking":
+    if directions_route == "driving" or "bicycling" or "walking" or "transit":
         print("You are", directions_result[0]['legs'][0]['distance']['text'], "from your destination."
         " Your commute time will be", directions_result[0]['legs'][0]['duration']['text'],".")
-        break
-    if directions_route == "transit":
-        print("You are", directions_result[0]['legs'][0]['distance']['text'], "from your destination."
-        " Your commute time will be", directions_result[0]['legs'][0]['duration']['text'],"."
-        " You will arrive at", directions_result[0]['legs'][0]['arrival_time']['text'], ".") #NEED TO FIX. NOTHING APPEARS
         break
     else:
         print("The travel mode was incorrectly entered. Please try again: ")
@@ -100,7 +90,7 @@ if response_key["cod"] != "404":
     weather_output = print("The current temperature is " + str(temp_format(kelvin2fahrenheit)) + " degrees Fahrenheit" +
     " with " + str(describe_weather) + ".")
 else:
-    print(".zip code not found. Please try again: ")
+    print("ZIP code not found. Please try again: ")
 
 #News
 print("\n")
@@ -126,61 +116,3 @@ def NYTNews():
 
 if __name__ == '__main__': 
      NYTNews()  #invoke the function
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import json, urllib, urllib.parse
-# from urllib.parse import urlencode
-# import googlemaps
-# start = "Bridgewater, Sa, Australia"
-# finish = "Stirling, SA, Australia"
-# 
-# url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
-#             ('origin', start),
-#             ('destination', finish)
-#  ))
-# ur = urllib.request.urlopen(url)
-# result = json.load(ur)
-# 
-# for i in range (0, len (result['routes'][0]['legs'][0]['steps'])):
-#     j = result['routes'][0]['legs'][0]['steps'][i]['html_instructions'] 
-#     print (j)
-
-
-#create map
-# import gmaps
-# import googlemaps
-# import pandas
-# import os
-# import gmaps.datasets
-# gmaps.configure(api_key2= api_key)
-#  
-#  
-# with open('apikey.txt') as f:
-#      api_key = f.readline()
-#      f.close
-#  
-# gmaps = googlemaps.Client(key='AIzaSyDaWK8dgNvwiKcxL5RF8xefSlhmzx9XsEQ 	')
-#  
-# new_york_coordinates = (40.75, -74.00)
-# fig = gmaps.figure(center=new_york_coordinates, zoom_level=12)
-# fig 
